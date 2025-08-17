9557b16cc3cf173b988a1082c0a07ddd75f224db
Hugo <104373242+HugoMS655@users.noreply.github.com>
2025-08-17 17:40:31 +0100

    Add global variables to keep track of scoring

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,3 +1,10 @@
+/*  These variables are intended to
+keep track of the scores for the human player and the computer player in a game of
+rock-paper-scissors. 
+The scores can be updated throughout the game as the players win rounds. */
+const humanScore = 0;
+const computerScore = 0;
+
 /**
  * The function `getComputerChoice` generates a random choice of "rock", "paper", or "scissors" for the
  * computer using a random number between 1 and 3 for the three choices
@@ -20,6 +27,13 @@ function getComputerChoice() {
   }
 }
 
+/**
+ * The function `getHumanChoice` prompts the user to input their choice of "rock", "paper", or
+ * "scissors", validates the input, and returns the sanitized choice.
+ * @returns The function `getHumanChoice()` returns the human's choice of "rock", "paper", or
+ * "scissors" after validating and processing the input provided by the user. If the input is invalid
+ * or if the user cancels the input prompt, the function returns `null`.
+ */
 function getHumanChoice() {
   let humanChoice = prompt("What is your choice? (rock, paper, or scissors)");
 
